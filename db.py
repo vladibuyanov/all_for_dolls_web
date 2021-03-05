5ae7fc4c5e9941965680e8cd8f003e4f2b32363c
Vladi <vladibuyanov@gmail.com>
2021-03-05 17:35:10 +0100

    0.1

---
+++ db.py
@@ -0,0 +1,20 @@
+db = [
+    {'src': './static/img/blythe_white.jpg',
+     'about': 'Standard white shoe mold for Blythe dolls. Use for the production of boots.',
+     'size': ' • Size - 2.5 cm.',
+     'price': '• Price - 2 € without delivery.',
+     'link': "https://www.instagram.com/p/CLEzKxmDSnw/?utm_source=ig_web_copy_link"
+     },
+
+    {'src': './static/img/paola_white.jpg',
+     'about': 'Standard white shoe mold for Blythe dolls. Use for the production of boots.',
+     'size': ' • Size - 2.5 cm.',
+     'price': '• Price - 2 € without delivery.',
+     'link': "https://www.instagram.com/p/CLw_tSyD1FZ/?utm_source=ig_web_copy_link"},
+
+    {'src': './static/img/handmade_green.jpeg',
+     'about': 'Standard green shoe mold for Handmade dolls. Use for the production of boots.',
+     'size': ' • Size - 2.5 cm. - 10 cm.',
+     'price': '• Price - 2.5 € - 17 € without delivery.',
+     'link': "https://www.instagram.com/p/CK7UEkGDZgQ/?utm_source=ig_web_copy_link"}
+]
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from db import db
+
 
 app = Flask(__name__)
 
@@ -6,7 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', db=db)
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def index_test():
+    return render_template('test.html', db=db)
 
 
 app.run()
